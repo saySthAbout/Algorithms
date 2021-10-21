f07ef0a3f67eed795cd65d057f321c53b395ea3e
saySthAbout <saysthabout@gmail.com>
2021-10-22 05:34:54 +0900

    Spring initializr로 Spring 프로젝트 생성하기
    기존의 쓸데없는 파일들 삭제

---
+++ Pile/src/test/java/Algorithm/Pile/PileApplicationTests.java
@@ -0,0 +1,13 @@
+package Algorithm.Pile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
